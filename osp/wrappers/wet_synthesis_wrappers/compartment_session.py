59576e9dbfd808d8be00c2850eb641af5445132b
AndreaQuerio <andrea.querio96@gmail.com>
2023-01-13 08:52:19 +0100

    Testing

--- osp/wrappers/wet_synthesis_wrappers/compartment_session.py
+++ osp/wrappers/wet_synthesis_wrappers/compartment_session.py
@@ -223,12 +223,10 @@ class CompartmentSession(SimWrapperSession):
         replace_char_in_keys(dataDict, "_", self._input_format["sep"])
 
         # Write the inputs in the include file
-        compDict = dataDict
-        print(dataDict, compDict)
+        self._update_files(dataDict)
+
         self._write_dict(
             dataDict, "input", "cfdSimulation/include", "cfdSimulation/include_original")
-
-        self._update_files(compDict)
 
         # self._add_division()
 
