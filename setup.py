44a4e72e793025ee73262bc5d1bf3e52a0903e1f
José Manuel Domínguez <jose.manuel.dominguez@iwm.fraunhofer.de>
2023-01-25 17:51:25 +0100

    Undo controlDict and installation changes as they will be merged in PRs #8 and #9 instead

--- setup.py
+++ setup.py
@@ -11,11 +11,9 @@ setup(
     name=NAME,
     version=VERSION,
     author='MuSyChEn research group at Politecnico di Torino',
-    description='A collection of wrappers for wet-phase synthesis of Ni-Mn-Co '
-                'hydroxide',
+    description='A collection of wrappers for wet-phase synthesis of Ni-Mn-Co hydroxide',
     keywords='SimDOME, CUDS, Precipitation, NMC hydroxide',
     long_description=README_TEXT,
-    packages=find_packages(exclude=("examples", "tests")),
     install_requires=[
         'osp-core>=' + OSP_CORE_MIN + ', <' + OSP_CORE_MAX,
         "graphviz",
@@ -26,9 +24,8 @@ setup(
         "mpi4py",
         "matplotlib",
         "mkl-service",
-    ],
+        ],
     packages=find_packages(exclude=("examples")),
-    python_requires=">=3.7",
     include_package_data=True,
     # entry_points={
     #     'wrappers':
